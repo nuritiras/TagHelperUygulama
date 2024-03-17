6fbab1361bd28bad1f33528c13e723c6a81e55da
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-03-18 00:01:54 +0300

    10. Uygulama

--- TagHelperUygulama/Models/Kullanici.cs
+++ TagHelperUygulama/Models/Kullanici.cs
@@ -4,15 +4,22 @@ namespace TagHelperUygulama.Models
 {
     public class Kullanici
     {
+        [Display(Name = "Ad Soyad")]
+        public string AdSoyad { get; set; }
         [EmailAddress]
+        [Display(Name = "Eposta Adresi")]
         public string EPosta { get; set; }
-        [DataType(DataType.DateTime)]
+        [DataType(DataType.Date)]
+        [Display(Name = "Doğum Tarihi")]
         public DateTime DogumTarihi { get; set; }
         [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Telefon Numarası")]
         public string Telefon { get; set; }
         [DataType(DataType.PostalCode)]
+        [Display(Name = "Posta Kodu")]
         public string PostaKodu { get; set; }
         [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
         public string Sifre { get; set; }
     }
 }
