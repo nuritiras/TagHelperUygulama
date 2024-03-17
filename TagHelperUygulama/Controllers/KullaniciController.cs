09606f37640ae980a48b0ab5252a08b76479dce6
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2024-03-17 23:54:50 +0300

    Add project files.

---
+++ TagHelperUygulama/Controllers/KullaniciController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TagHelperUygulama.Controllers
+{
+    public class KullaniciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Ekle()
+        {
+            return View();
+        }
+    }
+}
---
+++ TagHelperUygulama/Models/Kullanici.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TagHelperUygulama.Models
+{
+    public class Kullanici
+    {
+        [EmailAddress]
+        public string EPosta { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime DogumTarihi { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string Telefon { get; set; }
+        [DataType(DataType.PostalCode)]
+        public string PostaKodu { get; set; }
+        [DataType(DataType.Password)]
+        public string Sifre { get; set; }
+    }
+}
